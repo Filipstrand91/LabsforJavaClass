6f3379ed47132e78dc11773554ac276cd4a8c6c1
Filipstrand91 <filipstrand2@hotmail.com>
2018-02-06 21:15:34 +0100

    asdasd

--- Lab03/src/Graphics.java
+++ Lab03/src/Graphics.java
@@ -2,6 +2,11 @@ import se.lth.cs.pt.window.SimpleWindow;
 public class Graphics {
 	//public static void matin(String[] args) {
 		private SimpleWindow w = new SimpleWindow(300, 500, "Digging");
+		private int width;
+		private int blockSize;
+		private int height;
+			new SimpleWindow
+		
 			public void square () {
 				w.moveTo(10, 10);
 				w.lineTo(10, 20);
--- Lab03/src/Mole2.java
+++ Lab03/src/Mole2.java
@@ -1,8 +1,9 @@
 
 public class Mole2 {
 	public static void main(String[] args) {
-		//Graphics g = new Graphics();
+		Graphics g = new Graphics();
 		g.square();
+		// Vi sätter klassen Graphics till variablen g genom Graphic g = new Graphic(); sen kör vi g genom att skriva g.square();
 	}
 
 }
